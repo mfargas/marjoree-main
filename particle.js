9900707aef3957fc8bba426d32d24b42ee382727
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-03-23 16:40:22 -0700

    Testing particle simulator

---
+++ particle.js
@@ -0,0 +1,64 @@
+var started = false;
+
+var attractor;
+var particles = [];
+
+function setup() {
+
+  createCanvas(windowWidth, windowHeight);
+
+  var a = 1.25;
+
+  var b = 1.25;
+
+  for (var i = 0; i < 100; i++) {
+    particles.push(new Particle((windowWidth / 4), (windowHeight * 1 / 4)));
+  }
+  attractor = createVector(((a) * 300), ((b) * 300));
+  background(10, 10, 10);
+}
+
+
+function draw() {
+  stroke(300);
+  strokeWeight(0);
+  point(attractor.x, attractor.y);
+
+  for (var i = 0; i < 50; i++) {
+
+    var particle = particles[i];
+    particle.attracted(attractor);
+    particle.update();
+    particle.show();
+  }
+
+}
+
+
+function Particle(x, y) {
+  this.pos = createVector(x, y);
+  this.vel = p5.Vector.random2D();
+  this.acc = createVector();
+  this.lifespan = 2250.0;
+
+  this.update = function() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.lifespan--;
+  }
+
+  this.show = function() {
+    stroke(200, this.lifespan);
+    strokeWeight(1);
+    point(this.pos.x, this.pos.y);
+  }
+  this.attracted = function(target) {
+    var force = p5.Vector.sub(target, this.pos);
+    var dsquared = force.magSq();
+    dsquared = constrain(dsquared, 50, 400);
+    var G = 50;
+    var strength = G / dsquared;
+    force.setMag(strength);
+    this.acc = force;
+  }
+}
