affa01ee497420ebe1dfc437b0a167af292a63e0
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-04-16 23:37:53 -0700

    Adjust toggle button

--- menu-toggle.js
+++ menu-toggle.js
@@ -2,33 +2,10 @@ let menu = document.querySelector(".menu");
 let ham = document.querySelector(".menu-button");
 let bars = document.querySelector(".bars");
 let xSymbol = document.querySelector(".xSymbol");
-
-ham.addEventListener("click", toggleMenu);
+const menuItems = document.querySelector(".nav-link");
 
 function toggleMenu(){
-    if(ham.classList.contains("showMenu")){
-        menu.classList.remove("showMenu");
-        bars.style.display = "none";
-        xSymbol.style.display = "block";
-    } else {
-        menu.classList.add("showMenu");
-        bars.style.display = "block";
-        xSymbol.style.display = "none";
-    }
+    menuItems.forEach(nav => nav.classList.toggle('nav-toggle-showing'));
 }
 
-function showMenu(){
-    if(menu.style.display == "none"){
-        menu.style.display = "block";
-    } else {
-        menu.style.display = "none";
-    }
-};
-
-let menuLinks = document.querySelector(".nav-link");
-
-menuLinks.forEach(
-    function(menuLink){
-        menuLink.addEventListener("click", toggleMenu);
-    }
-)
+ham.addEventListener('click', toggleMenu);
