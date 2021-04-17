ec0d77cac8e913ad0b6652e6bd709f81ea603478
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-04-16 22:29:39 -0700

    Adjustment to button

--- menu-toggle.js
+++ menu-toggle.js
@@ -1,23 +1,31 @@
-var menu = document.querySelector(".menu");
-var ham = document.querySelector(".menu-button");
-var bars = document.querySelector(".bars");
-var xSymbol = document.querySelector(".xSymbol");
+let menu = document.querySelector(".menu");
+let ham = document.querySelector(".menu-button");
+let bars = document.querySelector(".bars");
+let xSymbol = document.querySelector(".xSymbol");
 
 ham.addEventListener("click", toggleMenu);
 
 function toggleMenu(){
     if(ham.classList.contains("showMenu")){
-        menu.classList.remove("showMe");
+        menu.classList.remove("showMenu");
         bars.style.display = "none";
         xSymbol.style.display = "block";
     } else {
-        menu.classList.add("showMe");
+        menu.classList.add("showMenu");
         bars.style.display = "block";
         xSymbol.style.display = "none";
     }
 }
 
-var menuLinks = document.querySelector(".nav-link");
+function showMenu(){
+    if(menu.style.display == "none"){
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+};
+
+let menuLinks = document.querySelector(".nav-link");
 
 menuLinks.forEach(
     function(menuLink){
