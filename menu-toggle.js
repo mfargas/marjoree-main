da4dd257d95d94ac2e67d89440eb6051fc69fdf1
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-04-17 10:56:46 -0700

    Different toggle js method

--- menu-toggle.js
+++ menu-toggle.js
@@ -4,16 +4,22 @@ let bars = document.getElementsByClassName("bars");
 let xSymbol = document.getElementsByClassName("xSymbol");
 const menuItems = document.getElementsByClassName("nav-link");
 
-function toggleMenu() {
-    (menu.classList.contains('active')) ? menu.classList.remove('active') : menu.classLIst.add('active');
-}
+// function toggleMenu() {
+//     (menu.classList.contains('active')) ? menu.classList.remove('active') : menu.classLIst.add('active');
+// }
 
-ham.addEventListener('click', function (e) {
-    e.preventDefault();
-});
+// ham.addEventListener('click', function (e) {
+//     e.preventDefault();
+// });
 
-for (var i = 0; i < menuItems.length; i++) {
-    menuItems[i].addEventListener('click', function () {
-        toggleMenu();
+for (var i = 0; i < ham.length; i++) {
+    ham[i].addEventListener('click', function () {
+        this.classList.toggle('active');
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
     });
 }
