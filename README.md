21bc970d6925f5e68d8227725b1d3ae89b815efe
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-05-02 12:36:35 -0700

    Update menu-toggle

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# Portfolio Site hosted on cPanel
+# Marjoree's Portfolio
 
-Bootstrap + React
+Welcome! Since the pandemic started, my goal was to build build build. The best place to not only showcase my skills but show my design capabilities as well.
 
 ## Intro
 
-Through inmotionhosting and cPanel, I've launched my personal portfolio. Primarily using HTML5, CSS3, JavaScript ES6, and JSX, I've encompassed the use of two frameworks.
+Through inmotionhosting and cPanel, I've launched my personal portfolio. Primarily using HTML5, CSS3, JavaScript ES6, and libraries such as jQuery in the structure of the site.
 
 ## Frameworks Used
 
-Bootstrap 5.0x and React Components are used to create my custom portfolio site. 
+I initially started with Bootstrap 5 and decided to start from scratch and implement components all on my own.
--- menu-toggle.js
+++ menu-toggle.js
@@ -1,25 +1,3 @@
-let menu = document.getElementsByClassName("menu-right");
-let ham = document.getElementsByClassName("menu-button");
-let bars = document.getElementsByClassName("bars");
-let xSymbol = document.getElementsByClassName("xSymbol");
-const menuItems = document.getElementsByClassName("nav-link");
-
-// function toggleMenu() {
-//     (menu.classList.contains('active')) ? menu.classList.remove('active') : menu.classLIst.add('active');
-// }
-
-// ham.addEventListener('click', function (e) {
-//     e.preventDefault();
-// });
-
-for (var i = 0; i < ham.length; i++) {
-    ham[i].addEventListener('click', function () {
-        this.classList.toggle('active');
-        var content = this.nextElementSibling;
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-        }
-    });
-}
+$(".icon").click(function () {
+    $(".menu-right").toggle( visibility );
+});
