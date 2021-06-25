880b5ad58bd89b0a917cec018fe410fc6341ecaa
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-06-24 22:15:24 -0700

    Test

--- emailForm.js
+++ emailForm.js
@@ -8,7 +8,7 @@ form.addEventListener("submit", (event) => {
 
     // Use your API endpoint URL you copied from the previous step
     const endpoint =
-        "https://8sn1mbfzv8.execute-api.us-west-1.amazonaws.com/Prod";
+        "https://rugz2zkg2b.execute-api.us-west-1.amazonaws.com/default";
     // We use JSON.stringify here so the data can be sent as a string via HTTP
     const body = JSON.stringify({
         senderName: name.value,
