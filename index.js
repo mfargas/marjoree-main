e00284d0a3c9c61423e6aedccfcd200db58d1cdb
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-07-21 11:55:52 -0700

    Attempt to structure db and routers

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const express = require('express');
 const app = express();
+var port = process.env.PORT || 8080;
 
-app.get('/html/about.html', function(req, res){
-    res.redirect('/about');
-});
+app.get('/', function(req, res) {res.send('Welcome')});
+
+app.listen(port, function () {
+    console.log("Running on Port " + port);
+})
+
+let apiRoutes = require("./router");
+
+app.use('/api', apiRoutes)
+
+let mongoose = require('mongoose');
+const router = require('router');
+let bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+// var about = '/html/about.html';
+
+
+
+// router.get('/', function (req, res) {
+//     console.log('hello')
+// });
+
+// router.get('/html/about.html', function(req, res){
+//     // var slug = req.param("slug");
+//     res.send('about')
+// });
---
+++ router.js
@@ -0,0 +1,9 @@
+let router = require('express').Router();
+
+router.get('/', function (req, res) {
+    res.json({
+        status: 'API Works',
+        message: 'Welcome to FirstRest API'
+    });
+});
+module.exports = router;
